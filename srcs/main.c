9f2d0abfca230de42dc78bf87ffc8cc1cba9918e
Gabriel Collet <gcollet@c2r4p5.42quebec.com>
2021-08-03 11:48:20 -0400

    version fonctionnel

--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,11 @@
 /*   By: gcollet <gcollet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 09:54:02 by gcollet           #+#    #+#             */
-/*   Updated: 2021/08/02 17:01:58 by gcollet          ###   ########.fr       */
+/*   Updated: 2021/08/03 11:46:39 by gcollet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
-
 
 void error()
 {
@@ -57,56 +56,74 @@ char *find_path(char *cmd, char **envp)
 	free_all(&paths);
 }
 
-//check permission with access for files because if it cant be open it will send back erno
-//valider que ca fait bien une erreur si le fichier de lecture n'existe pas
+void	child_process1(char **argv, char **envp, int *fd)
+{
+	int filein;
+	char **cmd;
+	
+	filein = open(argv[1], O_RDONLY, 0777);
+	if (filein == -1)
+		error();
+	dup2(fd[1], STDOUT_FILENO);
+	dup2(filein, STDIN_FILENO);
+	close(fd[0]);
+	close(fd[1]);
+	cmd = ft_split(argv[2], ' ');
+	if (execve(find_path(cmd[0], envp), cmd, envp) == -1)
+		error();
+	free_all(&cmd);
+}
+
+void	child_process2(char **argv, char **envp, int *fd)
+{
+	int fileout;
+	char **cmd;
+	//child process 2
+	fileout = open(argv[4], O_WRONLY | O_CREAT, 0777);
+	if (fileout == -1)
+		error();
+	dup2(fd[0], STDIN_FILENO);
+	dup2(fileout, STDOUT_FILENO);
+	close(fd[0]);
+	close(fd[1]);
+	cmd = ft_split(argv[3], ' ');
+	if (execve(find_path(cmd[0], envp), cmd, envp) == -1)
+		error();
+	free_all(&cmd);
+}
+
 
 int main(int argc, char **argv, char **envp)
 {
 	int fd[2];
 	int pid1;
 	int pid2;
-	char **cmd;
-	int file;
 	
-	argc = 0; //faire en sorte que si il y a pas assez d'arg display un error
 	
-	if (pipe(fd) == -1)
-		error();
-	pid1 = fork();
-	if (pid1 == -1)
-		error();
-	if(pid1 == 0)
+	if (argc >= 5)
 	{
-		//child process 1
-		dup2(fd[1], STDOUT_FILENO);
+		if (pipe(fd) == -1)
+			error();
+		pid1 = fork();
+		if (pid1 == -1)
+			error();
+		if(pid1 == 0)
+			child_process1(argv, envp, fd);
+		pid2 = fork(); //pk creer un fork apres ca fait ca?
+		if (pid2 == -1)
+			error();
+		if(pid2 == 0)
+			child_process2(argv, envp, fd);
+		}
 		close(fd[0]);
 		close(fd[1]);
-		cmd = ft_split(argv[1], ' ');
-		if (execve(find_path(cmd[0], envp), cmd, envp) == -1)
-			error();
-		free_all(&cmd);
+		waitpid(pid1, NULL, 0);
+		waitpid(pid2, NULL, 0);
 	}
-	pid2 = fork();
-	if (pid2 == -1)
-		error();
-	if(pid2 == 0)
+	else
 	{
-		//child process 2
-		file = open(argv[3], O_WRONLY | O_CREAT, 0777);
-		if (file == -1)
-			error();
-		dup2(fd[0], STDIN_FILENO);
-		dup2(file, STDOUT_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		cmd = ft_split(argv[2], ' ');
-		if (execve(find_path(cmd[0], envp), cmd, envp) == -1)
-			error();
-		free_all(&cmd);
+		printf("Error: Missing argument\n");
+		printf("Ex: ./pipex file1 cmd1 cmd2 file2\n");	
 	}
-	close(fd[0]);
-	close(fd[1]);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
 	return (0);
 }
